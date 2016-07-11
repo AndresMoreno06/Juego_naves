e0670407fff9b14209d21680ebcbb66c6c0dd7b1
Andres Moreno <andresmorenomora.06@gmail.com>
2016-07-11 14:25:06 -0400

    se crea clase y se dibuja la nave con caracteres

--- nave.cpp
+++ nave.cpp
@@ -58,35 +58,69 @@ void Ocultar(){
 	SetConsoleCursorInfo(hCon,&cci);
 }
 
+class NAVE{
+	/*
+		en una clase en poner cualquier atributo de esta manera , son automaticamente
+	  	privados
+	*/
+	
+	int x,y;
+public:
+	/*
+		Se crean las funciones publicar y su constructor
+	*/
+	NAVE (int _x,int _y): x (_x),y(_y){};
+	void Pintar();
+	void Borrar();
+	void Mover();
+
+};
+
+/*
+	Asi de estructura la funcion de una clase
+*/
+void NAVE::Pintar(){
+	gotoxy(x,y);printf("  %c",30);
+	gotoxy(x,y+1);printf(" %c%c%c",40,207,41);
+	gotoxy(x,y+2);printf("%c%c %c%c",30,190,190,30);
+}
+
+void NAVE::Borrar(){
+	gotoxy(x,y);printf("     ");
+	gotoxy(x,y+1);printf("     ");
+	gotoxy(x,y+2);printf("     ");
+}
+
+void NAVE::Mover(){
+	/*
+		Kbhit() es una funcion que verifica si se preciono una tecla
+	*/ 
+	if(kbhit()){
+		
+		char tecla = getch();
+		Borrar();
+		//Cuando se presiona para derecha
+		if(tecla == DERECHA)x--;
+		//Cuando se presiona para izquierda
+		if(tecla == IZQUIERDA)x++;
+		//Cuando se presiona para arriba
+		if(tecla == ARRIBA)y--;
+		//Cuando se presiona para abajo
+		if(tecla == ABAJO)y++;
+	 	Pintar();		
+	}
+}
+
 int main(){
 	
 	Ocultar();
- 	int x = 2 , y = 2;
- 	gotoxy(x,y);
- 	printf("*");
- 	
+	NAVE N(7,7);
+ 	N.Pintar();
  	bool game_over = false;
  	while(!game_over){
- 		/*
-			Kbhit() es una funcion que verifica si se preciono una tecla
-		*/ 
-		if(kbhit()){
- 			
- 			char tecla = getch();
- 			gotoxy(x,y);
- 			printf(" ");
- 			//Cuando se presiona para derecha
-			if(tecla == DERECHA)x--;
-			//Cuando se presiona para izquierda
-			if(tecla == IZQUIERDA)x++;
-			//Cuando se presiona para arriba
-			if(tecla == ARRIBA)y--;
-			//Cuando se presiona para abajo
-			if(tecla == ABAJO)y++;
-		 	gotoxy(x,y);
-		 	printf("*");
- 			
-		}
+ 		
+ 		N.Mover();
+ 		Sleep(30);
 	 }
  	
  	return 0;
